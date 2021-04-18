63169ec5d00606e5eab3628785715871a8d1037c
IoannP <giohann1990@gmail.com>
2021-04-19 00:55:21 +0300

    add label routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ import users from './users.js';
 import session from './session.js';
 import statuses from './statuses.js';
 import tasks from './tasks.js';
+import label from './label.js';
 
-const controllers = [welcome, users, session, statuses, tasks];
+const controllers = [
+  welcome,
+  users,
+  session,
+  statuses,
+  tasks,
+  label,
+];
 
 export default (app) => controllers.forEach((f) => f(app));
