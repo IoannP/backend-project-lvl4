574662cf5f0353708318c1508e6ca334ab05e096
IoannP <giohann1990@gmail.com>
2021-05-05 09:25:15 +0300

    debug database

--- knexfile.js
+++ knexfile.js
@@ -27,9 +27,13 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://hbeeepvnlxgqnd:5ee9d49ae3ebb2cdda344d078fc2ca801112b34a2e92881ad616137a4bc4c71f@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/ddt3ld9cics00d',
-    ssl: {
-      rejectUnauthorized: false,
+    connection: {
+      host: process.env.PROD_DB_HOST,
+      port: process.env.PROD_DB_PORT,
+      user: process.env.PROD_DB_USER,
+      password: process.env.PROD_DB_PASSWORD,
+      database: process.env.PROD_DB,
+      databaseUrl: 'postgres://hbeeepvnlxgqnd:5ee9d49ae3ebb2cdda344d078fc2ca801112b34a2e92881ad616137a4bc4c71f@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/ddt3ld9cics00d',
     },
     useNullAsDefault: true,
     migrations,
